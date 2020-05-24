b5d2fe04bc4a4a0fe39efb311312cfe9a6e355e4
Marco Antonio Melo <marcomg_777@outlook.com>
2020-05-24 17:57:34 -0500

    Creación del template inicial de la app

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,60 @@
 <template>
-  <v-app>
+  <v-app id="app">
+
     <v-app-bar
       app
-      color="primary"
+      clipped-right
+      color="transparent"
       dark
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer">
+        <v-icon>fas fa-bars</v-icon>
+      </v-app-bar-nav-icon>
     </v-app-bar>
 
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      clipped
+      color="transparent"
+    >
+      <v-list dense>
+        <v-list-item @click.stop="drawer = !drawer">
+          <v-list-item-action>
+            <v-icon style="color: white">fas fa-home</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title style="color: white">Home</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
     <v-content>
-      <HelloWorld/>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+
   },
 
   data: () => ({
-    //
+    drawer: false,
   }),
 };
 </script>
+
+<style scoped>
+.appBar{
+  background-color: transparent;
+}
+#app{
+  background-image: linear-gradient(191deg, rgba(35,148,160,1) 44%, rgba(168,180,16,1) 100%);
+}
+</style>
