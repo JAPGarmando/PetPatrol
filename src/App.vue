63a23a634412d9bf0eabee67190c8f356f3dba9e
Marco Antonio Melo <marcomg_777@outlook.com>
2020-05-24 19:01:12 -0500

    Deestructuración de app en componentes

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,40 @@
 <template>
   <v-app id="app">
 
-    <v-app-bar
-      app
-      clipped-right
-      color="transparent"
-      dark
-    >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer">
-        <v-icon>fas fa-bars</v-icon>
-      </v-app-bar-nav-icon>
-    </v-app-bar>
+    <application-bar/>
 
-    <v-navigation-drawer
-      v-model="drawer"
-      app
-      clipped
-      color="transparent"
-    >
-      <v-list dense>
-        <v-list-item @click.stop="drawer = !drawer">
-          <v-list-item-action>
-            <v-icon style="color: white">fas fa-home</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title style="color: white">Home</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
+    <navigation-drawer/>
 
     <v-content>
+      <router-view/>
     </v-content>
   </v-app>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+import navigationDrawer from './components/navDrawer.vue'
+import applicationBar from './components/appBar.vue'
 
 export default {
   name: 'App',
 
   components: {
-
+    navigationDrawer,
+    applicationBar
   },
 
   data: () => ({
     drawer: false,
   }),
+
+  computed: {
+    ...mapState(['navDrawer'])
+  }
 };
 </script>
 
 <style scoped>
-.appBar{
-  background-color: transparent;
-}
 #app{
   background-image: linear-gradient(191deg, rgba(35,148,160,1) 44%, rgba(168,180,16,1) 100%);
 }
---
+++ src/components/appBar.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-app-bar
+      app
+      clipped-right
+      color="transparent"
+      class="appBar"
+      dark
+    >
+        <v-app-bar-nav-icon @click.stop="navDrawer.drawer = !navDrawer.drawer">
+            <v-icon>fas fa-bars</v-icon>
+        </v-app-bar-nav-icon>
+        <v-spacer></v-spacer>
+        <div class="logo"></div>
+        <v-spacer></v-spacer>
+        <v-app-bar-nav-item>
+            <v-icon>fas fa-search</v-icon>
+        </v-app-bar-nav-item>
+    </v-app-bar>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+    name: 'appBar',
+    computed: {
+        ...mapState(['navDrawer'])
+    }
+}
+</script>
+
+<style scoped>
+    .appBar{
+        background-color: transparent;
+        border-bottom: white 1px;  
+    }
+    .logo{
+        width: 200px;
+        height: 72.64px;
+        padding: 0px;
+        background-size: contain;
+        background-image: url('../assets/logoPetPatrol.png');
+    }
+</style>
